5e6d2f94453c47b298de001665cd01453637fadc
freewind <nowindlee@gmail.com>
2019-04-26 22:28:01 +0800

    improve

--- trimmedEqual.ts
+++ trimmedEqual.ts
@@ -6,11 +6,11 @@ function trimmedEqual() {
     const actual = this._obj.trim();
     const expected = expectedString.trim();
     this.assert(
-      actual === expected
-      , 'expected #{this} to equal string #{exp} after trimmed, but it was #{act} after trimmed'
-      , 'expected #{this} not to equal string #{exp} after trimmed'
-      , expected
-      , actual
+      actual === expected,
+      'expected #{this} to equal string #{exp} after trimmed, but it was #{act} after trimmed',
+      'expected #{this} not to equal string #{exp} after trimmed',
+      expected,
+      actual,
     );
   });
 }
